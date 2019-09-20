a72198c1164450b9c32c6ad92303ae4750b1d65b
Emory Walsh <ewalsh00@stuy.edu>
2019-09-20 12:11:05 -0400

    Now displays random job. Need to create a list of all now.

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -3,35 +3,63 @@
 #K10 -- Jinja Tuning
 #2019-09-20
 
-import random
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/occupyflaskft")
-def occs():
-    render_template("tmplt.html")
 
-    #create dictionary based on csv
-    dict = {} #initialize a new dictionary structure
-    file = open("occupations.csv", "r") #open up csv file for reading purposes
-    content = file.readlines() #parse through file by lines
-    content = content[1:len(content) - 1] #take out the first and last line
-    for line in content:
-        line = line.strip() #removes \n
-        line = line.lstrip('\"') #removes leading quote
-        if ("\"" in line): #if line contains quotation marks
-            line = line.split("\",") #splits line by ",
-        else:
-            line = line.split(",") #if line does not contain quotes, split by comma
+@app.route("/")
+def norm():
+    return "normal"
 
-        dict[line[0]] = float(line[1]) #key value pair
-    #print(dict) #testing results
-    file.close()
+coll = [0, 1, 1, 2, 3, 5, 8]
 
-    def randJob():
-        list = []
-        for key, value in dict.items(): #found this iteration on stack overflow
-            list += [key] * int(value * 10) #big thanks to Grace for this alternative to a forward loop
-        #print(list)
-        #print(len(list)) #should return 998
-        return random.choice(list)
+
+def norm():
+    return "Go to other page"
+
+
+#create dictionary based on csv
+dict = {} #initialize a new dictionary structure
+file = open("occupations.csv", "r") #open up csv file for reading purposes
+content = file.readlines() #parse through file by lines
+content = content[1:len(content) - 1] #take out the first and last line
+for line in content:
+    line = line.strip() #removes \n
+    line = line.lstrip('\"') #removes leading quote
+    if ("\"" in line): #if line contains quotation marks
+        line = line.split("\",") #splits line by ",
+    else:
+        line = line.split(",") #if line does not contain quotes, split by comma
+
+    dict[line[0]] = float(line[1]) #key value pair
+#print(dict) #testing results
+file.close()
+
+import random
+
+def randJob():
+    list = []
+    for key, value in dict.items(): #found this iteration on stack overflow
+        list += [key] * int(value * 10) #big thanks to Grace for this alternative to a forward loop
+    #print(list)
+    #print(len(list)) #should return 998
+    return random.choice(list)
+
+
+
+@app.route('/occupyflaskft')
+def occs_template():
+    print(randJob())
+    return render_template('tmplt.html',
+        ti = 'Occupations',
+        randJob = randJob(),
+        collection = coll
+        #collection = dict
+
+        )
+
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
