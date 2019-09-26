3ee8b85d2ecbd3b1722fb5639e0bc61b6a9d5367
Emory Walsh <ewalsh00@stuy.edu>
2019-09-26 08:41:39 -0400

    Assignment done

--- 9.25test/app.py
+++ 9.25test/app.py
@@ -5,18 +5,29 @@ import cgi
 
 app = Flask(__name__)
 
-@app.route("/")
-def norm():
-    print(app)
-    return "normal"
+head = """
+Emory Walsh & Justin Shaw
+SoftDev1 pd1
+K12 -- Echo Echo Echo
+2019-09-26
+"""
 
-@app.route("/form")
+@app.route("/")
 def form():
-    return render_template("temp.html")
+    return render_template("temp.html",
+                               header = head)
 
 @app.route("/auth")
 def authenticate():
-    return "form handled"
+    print("/n/n/n")
+    print("app: " + str(app))
+    print("request: " + str(request))
+    print("args: " + str(request.args))
+    print(request.headers)
+    return render_template("authTemp.html",
+                               username=request.args['username'],
+                               method = request.method,
+                               header = head)
 
 
 if __name__ == "__main__":
