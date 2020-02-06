3539e06d4fbb4a075acedabfba8862f9d03ee62f
Emory Walsh <ewalsh00@stuy.edu>
2020-02-05 20:12:50 -0500

    Finished and working

--- 04_canvas/clickdraw.js
+++ 04_canvas/clickdraw.js
@@ -1,5 +1,47 @@
-const canvas = document.getElementById('slate');
-const ctx = canvas.getContext('2d');
+//Emory Walsh
+//Softdev pd09
+//K04 -- I See a Red Door...
+//2020-02-05
 
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 150, 100);
+var canvas = document.getElementById("slate");
+var ctx = canvas.getContext("2d");
+
+//mode either "rect" or "dot"
+var curmode = "dot";
+
+canvas.addEventListener("click", (e) => {
+    //console.log("mode: " + curmode);
+    pgx = e.pageX;
+    pgy = e.pageY;
+    cx = pgx - canvas.offsetLeft;
+    cy = pgy - canvas.offsetTop;
+    if(curmode === "dot"){
+      ctx.fillStyle = "blue";
+      ctx.beginPath();
+      ctx.arc(cx, cy, 10, 0, 2 * Math.PI);
+      ctx.stroke();
+      ctx.fill();
+    }
+    else{
+      ctx.fillStyle = "pink";
+      ctx.fillRect(cx, cy, 10, 40);
+    }
+});
+
+document.getElementById("clear").addEventListener("click", () => {
+  //console.log("clear");
+  ctx.fillStyle = "white";
+  ctx.fillRect(0,0,canvas.width,canvas.height);
+});
+
+document.getElementById("mode").addEventListener("click", () => {
+  if(curmode === "rect"){
+    curmode = "dot";
+    //console.log("dot");
+  }
+  else{
+
+    curmode = "rect";
+    //console.log("rect2");
+  }
+});
