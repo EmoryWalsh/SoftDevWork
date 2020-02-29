aabf77c443afe6b6d6d0b08ad38b69360cf21c41
Emory Walsh <ewalsh00@stuy.edu>
2020-02-29 13:51:14 -0500

    Starting 10_mongo

---
+++ 10_mongo/movies.py
@@ -0,0 +1,19 @@
+#Emory Walsh & Leia Park
+#SoftDev1 pd9
+#K10 -- Import/Export Bank
+#2020-03-04
+
+import pymongo, json
+from bson.json_util import loads
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client['test']
+collection = db.movies
+
+#read data into db
+if(collection.count() == 0):
+    file = open("data.json", "r")
+    data = file.readlines()
+    for line in data:
+        collection.insert_one(loads(line))
