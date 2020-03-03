8db65d30e211e8a4a3592d95855bfa0ae496b85b
Emory Walsh <ewalsh00@stuy.edu>
2020-03-02 21:18:48 -0500

    White space gone

--- 10_mongo/main.py
+++ 10_mongo/main.py
@@ -9,47 +9,11 @@ from pymongo import MongoClient
 
 client = MongoClient()
 db = client['test']
-collection = db.movies
+collection = db.representatives
 
 #read data into db
-if(collection.count() == 0):
-    data = json.load(sys.stdin) # read json data from standard input
-    strip_whitespace(data)
-    json.dump(data, sys.stdout, indent=2)
-    file = open("reps.json", "r")
-    data = file.readlines()
-    data.strip('\n')
-    print data
-    for line in data:
+if(collection.count()==0):
+    file = open("primer-dataset.json", "r")
+    content = file.readlines()
+    for line in content:
         collection.insert_one(loads(line))
-
-
-
-import json
-import sys
-from pkgutil import simplegeneric
-
-@simplegeneric
-def get_items(obj):
-    while False: # no items, a scalar object
-        yield None
-
-@get_items.register(dict)
-def _(obj):
-    return obj.items() # json object. Edit: iteritems() was removed in Python 3
-
-@get_items.register(list)
-def _(obj):
-    return enumerate(obj) # json array
-
-def strip_whitespace(json_data):
-    for key, value in get_items(json_data):
-        if hasattr(value, 'strip'): # json string
-            json_data[key] = value.strip()
-        else:
-            strip_whitespace(value) # recursive call
-
-
-data = json.load(sys.stdin) # read json data from standard input
-strip_whitespace(data)
-json.dump(data, sys.stdout, indent=2)
