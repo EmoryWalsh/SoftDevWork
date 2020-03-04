c96c738aa3915f7b513a0631a8aa2cb44132867e
Emory Walsh <ewalsh00@stuy.edu>
2020-03-03 21:09:57 -0500

    Trying senator json

--- 10_monge/parse.py
+++ 10_monge/parse.py
@@ -1,17 +1,47 @@
-import pymongo, json
 from bson.json_util import loads
+import pymongo
 from pymongo import MongoClient
 
 client = MongoClient()
-db = client['test']
-collection = db.prize
+db = client.computers
 
-#read data into db
 if(collection.count()==0):
-    file = open("data.json", "r")
-    content = file.readlines()
+    collection = db.senators
+    file = open("senators", "r")
+    content = loads(file.read())["objects"]
     for line in content:
-        collection.insert_one(loads(line))
+        collection.insert_one(line)
 
-def prize_subject(subject):
-    return collection.find({"prizes.category" : subject}, {"_id" : 0, "laureates.firstname" : 1, "laureates.lastname" : 1})
+def find_gender(gender):
+    return senators.find({"person.gender" : gender}, {"_id" : 0, "person.name" : 1})
+
+def find_state(state):
+    return senators.find({"state" : state}, {"_id" : 0, "person.name" : 1})
+
+def find_party(party):
+    return senators.find({"party" : party}, {"_id" : 0, "person.name" : 1})
+
+def find_website(firstname):
+    return senators.find({"person.firstname" : firstname}, {"_id" : 0, "person.name" : 1, "website" : 1})
+
+def find_description(lastname):
+    return senators.find({"person.lastname" : lastname}, {"_id" : 0, "person.name" : 1, "description" : 1})
+
+
+
+
+print("-----FINDING ALL SENATORS IN NY-----")
+for item in find_state("NY"):
+    print(item["person"])
+
+print("-----FINDING ALL DEMOCRATIC SENATORS-----")
+for item in find_party("Democrat"):
+    print(item["person"])
+
+print("----FINDING ALL FEMALE SENATORS-----")
+for item in find_gender("female"):
+    print(item["person"])
+
+print("-----FINDING WEBSITE OF SENATORS WHOSE FIRST NAME IS KEVIN")
+for item in find_website("kevin"):
+    print(item["person"])
