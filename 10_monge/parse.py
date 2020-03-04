2d9a8affb47ecfa65015d4987e7d06e0d347811a
Emory Walsh <emoryleighwalsh@yahoo.com>
2020-03-04 02:26:19 +0000

    Few quick fixes

--- 10_monge/parse.py
+++ 10_monge/parse.py
@@ -4,32 +4,32 @@ from pymongo import MongoClient
 
 client = MongoClient()
 db = client.computers
+collection = db.senators
 
 if(collection.count()==0):
-    collection = db.senators
     file = open("senators", "r")
     content = loads(file.read())["objects"]
     for line in content:
         collection.insert_one(line)
 
-def ender(gender):
-    return senators.find({"person.gender" : gender}, {"person.name" : 1})
+def gender(gender):
+    return collection.find({"person.gender" : gender}, {"person.name" : 1})
 
 def state(state):
-    return senators.find({"state" : state}, {"person.name" : 1})
+    return collection.find({"state" : state}, {"person.name" : 1})
 
 def party(party):
-    return senators.find({"party" : party}, {"person.name" : 1})
+    return collection.find({"party" : party}, {"person.name" : 1})
 
 def website(fname):
-    return senators.find({"person.firstname" : fname}, {"person.name" : 1, "website" : 1})
+    return collection.find({"person.firstname" : fname}, {"person.name" : 1, "website" : 1})
 
 def description(lname):
-    return senators.find({"person.lastname" : lname}, {"person.name" : 1, "description" : 1})
+    return collection.find({"person.lastname" : lname}, {"person.name" : 1, "description" : 1})
 
 def printer(data):
-    for answer in data:
-        print(item[person])
+    for item in data:
+        print(item["person"])
 
 #Male representatives
 printer(gender("male"))
