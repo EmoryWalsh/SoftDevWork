e6f776243e3996955bc5779c715e27748d64f103
Emory Walsh <ewalsh00@stuy.edu>
2020-03-03 23:42:19 -0500

    Added comments

--- 10_mongo/parse.py
+++ 10_mongo/parse.py
@@ -7,7 +7,7 @@
 Dataset: Current US Senators
 Description: Contains data on all current US senators including personal information and political background
 Hyperlink: https://www.govtrack.us/api/v2/role?current=true&role_type=senator
-Import mechanism summary: By importing pymongo, json, and pprint, we gain access to the codes in those modules. If there are no collections present in our database, we read from the json file and load them into a collection.
+Import mechanism summary: By importing pymongo, we gain access to the codes in those modules. If there are no collections present in our database, we read from the json file and load them into a collection.
 """
 
 from bson.json_util import loads
@@ -15,51 +15,49 @@ import pymongo
 from pymongo import MongoClient
 
 client = MongoClient()
-db = client.computers
-collection = db.senators
+db = client.computers # creates a database
+collection = db.senators # adds a collection of senators
 
+# read the data
 if(collection.count()==0):
     file = open("senators", "r")
     content = loads(file.read())["objects"]
     for line in content:
         collection.insert_one(line)
 
+# gets US senators of specified gender
 def gender(gender):
     return collection.find({"person.gender" : gender}, {"person.name" : 1})
 
+# gets US senators from specified state
 def state(state):
     return collection.find({"state" : state}, {"person.name" : 1})
 
+# gets US senators from specified party
 def party(party):
     return collection.find({"party" : party}, {"person.name" : 1})
 
-def website(lname):
-    return collection.find({"person.firstname" : lname}, {"person.name" : 1, "website" : 1})
+# gets the website of a specific US senator
+def website(fname):
+    return collection.find({"person.firstname" : fname}, {"person.name" : 1, "website" : 1})
 
+# gets all related information of a specific US senator
 def description(lname):
     return collection.find({"person.lastname" : lname}, {"person.name" : 1, "description" : 1})
 
-def socialmedia(lname):
-    return collection.find({"person.lastname" : lname}, {"person.twitterid" : 1, "person.youtubeid" : 1})
-
-def birthday(lname):
-    return collection.find({"person.lastname" : lname}, {"person.birthday" : 1})
-
+# helper method
 def printer(data):
     for item in data:
         print(item["person"])
 
+# TESTS
 print("\nMale representatives")
 printer(gender("male"))
 print("\nDemocrats")
 printer(party("Democrat"))
 print("\nME senators")
 printer(state("ME"))
-print("\nElizabeth Warren's website")
-printer(website("Warren"))
+print("\nElizabeth's website")
+printer(website("Elizabeth"))
 print("\nAmy Klobuchar description")
 printer(description("Klobuchar"))
-print("\nKevin Cramer's social media")
-printer(socialmedia("Cramer"))
-print("\nMartha McSally birthday")
-printer(birthday("McSally"))
