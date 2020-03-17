b182ee46037bc3b499d8f9d790aeda07a0fe6cda
Emory Walsh <ewalsh00@stuy.edu>
2020-03-16 23:19:55 -0400

    Trying to make run

--- 11_mongoflask/app.py
+++ 11_mongoflask/app.py
@@ -12,7 +12,7 @@ db = client["test"]
 opioids_col = db["opioids"] #collection storing data from opioids dataset
 senators_col = db["senators"] #collection storing data from senators dataset
 
-def setup_opioids():
+'''def setup_opioids():
     #col.remove({})
     if opioids_col.count_documents({}) == 0:
         with open('opioids_data.json','r') as file:
@@ -21,7 +21,7 @@ def setup_opioids():
             lis = data["data"]
             for item in lis:
                 opioids_col.insert_one({"date": item[9], "datetype": item[10], "age": item[11], "sex": item[12], "race": item[13], "residencecity": item[14], "residencecounty": item[15], "residencestate": item[16], "deathcity": item[17], "deathcounty": item[18], "location": item[19], "locationifother": item[20], "descriptionofinjury": item[21], "injuryplace": item[22], "injurycity": item[23], "injurycounty": item[24], "injurystate": item[25], "cod": item[26], "othersignifican": item[27], "heroin": item[28], "cocaine": item[29], "fentanyl": item[30], "fentanylanalogue": item[31], "oxycodone": item[32], "oxymorphone": item[33],  "ethanol": item[34], "hydrocodone": item[35], "benzodiazepine": item[36], "methadone": item[37], "amphet": item[38], "tramad": item[39], "morphine_notheroin": item[40], "hydromorphone": item[41], "other": item[42], "opiatenos": item[43], "anyopioid": item[44], "mannerofdeath": item[45]})
-
+'''
 def setup_senators():
     # read the data
     if(senators_col.count_documents({})==0):
