72f8b7aeecd80a138cc9e4206384962e21f15e41
Emory Walsh <ewalsh00@stuy.edu>
2020-03-27 15:41:13 -0400

    Added lines

--- 12_svg-dot/svg.js
+++ 12_svg-dot/svg.js
@@ -5,16 +5,32 @@
 
 var svg = document.getElementById("vimage");
 
+lx = null
+ly = null
+
 svg.addEventListener("click", (e) => {
-  var circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
   x = e.pageX-3;
   y = e.pageY-20;
-  console.log(x)
+
+  var circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
   circle.setAttributeNS(null, 'cx', x);
   circle.setAttributeNS(null, 'cy', y);
   circle.setAttributeNS(null, 'r', 5);
-  circle.setAttributeNS(null, 'style', 'fill: none; stroke: blue; stroke-width: 1px;' );
+  circle.setAttributeNS(null, 'style', 'fill: blue; stroke: blue; stroke-width: 1px;' );
   svg.appendChild(circle);
+
+  if(lx != null && ly != null){
+    var line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+    line.setAttributeNS(null, 'x1', lx);
+    line.setAttributeNS(null, 'y1', ly);
+    line.setAttributeNS(null, 'x2', x);
+    line.setAttributeNS(null, 'y2', y);
+    line.setAttributeNS(null, 'style', 'fill: none; stroke: blue; stroke-width: 1px;' );
+    svg.appendChild(line);
+  }
+
+  lx = x
+  ly = y
 });
 
 document.getElementById("clear").addEventListener("click", () => {
