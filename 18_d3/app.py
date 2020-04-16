d496b2b607c2558b4affd0a79b73c226f27a2344
Emory Walsh <ewalsh00@stuy.edu>
2020-04-16 14:12:40 -0400

    Do not know what I'm doing

--- 18_d3/app.py
+++ 18_d3/app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+import csv
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return "Hello world!"
+    return render_template("index.html")
+
+@app.route("/data", methods={"GET"})
+def data():
+    data = []
+    with open("data.csv") as csv_file:
+        reader = csv.reader(csv_file)
+        # skip the first row, which is the header row
+        next(reader)
+        for entry in reader:
+            data.append({'reading': entry[3], 'math': int(entry[4]), 'writing': entry[5]})
+
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.debug = True
