7987ed0bccdf8cf7993ea7b0cbb495b3ef9d859f
Emory Walsh <ewalsh00@stuy.edu>
2020-04-21 11:39:43 -0400

    It half works now

--- 18_d3/static/stuff.js
+++ 18_d3/static/stuff.js
@@ -5,99 +5,136 @@
  * 2020-04-21
  */
 
-const width = 800;
-const height = 500;
+ const width = 800;
+ const height = 500;
 
-function reading(origData){
-  buckets = [0, 0, 0, 0, 0, 0, 0, 0]
-  for(let step = 0; step < origData.length; step++){
-    bucket = Math.floor(origData[step]["reading"] / 100)
-    //console.log(bucket)
-    buckets[bucket] += 1
-  }
-  //console.log(buckets)
-  return buckets
-}
+ d3.select("#read")
+   .on("click", () => {
+       //console.log("start")
+       makeGraph(reading(data))
+   });
 
-d3.select("#start")
-  .on("click", () => {
-      //console.log("start")
-      readingData = reading(data)
-      makeGraph(readingData)
-  });
+ d3.select("#math")
+   .on("click", () => {
+       //console.log("start")
+       makeGraph(math(data))
+   });
 
-function makeGraph(data){
-  data_viz.innerHTML = ""
-  // set the dimensions and margins of the graph
-  var margin = {top: 10, right: 30, bottom: 30, left: 40},
-      width = 460 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
-
-  // append the svg object to the body of the page
-  var svg = d3.select("#data_viz")
-    .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
-
-    // X axis: scale and draw:
-    var x = d3.scaleLinear()
-        .domain([0, 800])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-        .range([0, width]);
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-    // set the parameters for the histogram
-    var histogram = d3.histogram()
-        .value(function(d) { return d.price; })   // I need to give the vector of value
-        .domain(x.domain())  // then the domain of the graphic
-        .thresholds(x.ticks(8)); // then the numbers of bins
-
-    // And apply this function to data to get the bins
-    var bins = histogram(data);
-    console.log(data)
-    console.log(bins)
-
-    // Y axis: scale and draw:
-    var y = d3.scaleLinear()
-        .range([height, 0]);
-        y.domain([0, 300]);   // d3.hist has to be called before the Y axis obviously
-    svg.append("g")
-        .call(d3.axisLeft(y));
-
-    // append the bar rectangles to the svg element
-    svg.selectAll("rect")
-        .data(bins)
-        .enter()
-        .append("rect")
-          .attr("x", 1)
-          .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length - 20) + ")"; })
-          .attr("width", function(d) { return 100 ; })
-          .attr("height", function(d) )
-          .style("fill", "#69b3a2")
-}
+ d3.select("#write")
+   .on("click", () => {
+     //console.log("start")
+     makeGraph(writing(data))
+   });
 
 
+ function reading(origData){
+   reading = []
+   for(let step = 0; step < origData.length; step++){
+     reading.push(origData[step]["reading"])
+   }
+   return reading
+ }
 
-  fetch("/data")
-    .then((response) => response.json())
-    .then((entries) => {
-      //console.log(entries)
+ function math(origData){
+   math = []
+   for(let step = 0; step < origData.length; step++){
+     math.push(origData[step]["math"])
+   }
+   return math
+ }
 
-      // update global variable
-      data = entries;
+ function writing(origData){
+   writing = []
+   for(let step = 0; step < origData.length; step++){
+     writing.push(origData[step]["writing"])
+   }
+   return writing
+ }
 
-    })
-    .catch((e) => {
-        alert("Failed to get data from the server. See console for more information");
-        console.log(e)
-    });
+ function makeGraph(data){
+   console.log(data)
+   data_viz.innerHTML = ""
+
+   min = d3.min(data);
+   max = d3.max(data);
+   domain = [min,max];
+
+   var margin = { top: 30, right: 30, bottom: 30, left: 50 },
+     width = 460 - margin.left - margin.right,
+     height = 400 - margin.top - margin.bottom;
+
+   // The number of bins
+   Nbin = 10;
+
+   var x = d3
+     .scaleLinear()
+     .domain([0, 800])
+     .range([0, width]);
+
+   var histogram = d3
+     .histogram()
+     .domain(x.domain()) // then the domain of the graphic
+     .thresholds(x.ticks(Nbin)); // then the numbers of bins
+
+   // And apply this function to data to get the bins
+   var bins = histogram(data);
+
+   // Add the svg element to the body and set the dimensions and margins of the graph
+   var svg = d3
+     .select("body")
+     .append("svg")
+     .attr("width", width + margin.left + margin.right)
+     .attr("height", height + margin.top + margin.bottom)
+     .append("g")
+     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+   svg
+     .append("g")
+     .attr("transform", "translate(0," + height + ")")
+     .call(d3.axisBottom(x));
+
+   var y = d3
+     .scaleLinear()
+     .range([height, 0])
+     .domain([
+       0,
+       d3.max(bins, function(d) {
+         return d.length;
+       })
+     ]);
+
+   svg.append("g").call(d3.axisLeft(y));
+
+   svg
+     .selectAll("rect")
+     .data(bins)
+     .enter()
+     .append("rect")
+     .attr("x", 1)
+     .attr("transform", function(d) {
+       return "translate(" + x(d.x0) + "," + y(d.length) + ")";
+     })
+     .attr("width", function(d) {
+       return x(d.x1) - x(d.x0) - 1;
+     })
+     .attr("height", function(d) {
+       return height - y(d.length);
+     })
+     .style("fill", "#69b3a2");
+ }
 
 
-d3.select("#end")
-  .on("click", () => {
-      console.log("stop")
-  });
+
+ fetch("/data")
+   .then((response) => response.json())
+   .then((entries) => {
+   data = entries;
+
+ });
+
+
+ d3.select("#end")
+   .on("click", () => {
+     data_viz.innerHTML = ""
+     console.log("stop")
+ });
